d715c35ef913c7841e68e2c9745af27c5a335cf9
nahimulbs23 <nahimul.islam@brainstation-23.com>
2025-04-10 18:00:18 +0600

    Change the Special widget plugin directory

---
+++ src/Plugins/Nop.Plugin.Widgets.SpecialOffers/Models/SpecialOffersModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Nop.Plugin.Widgets.SpecialOffers.Models;
+public class SpecialOffersModel
+{
+    public class SpecialOfferModel
+    {
+        public string Title { get; set; }
+        public string Discount { get; set; }
+        public int ProductId { get; set; }
+        public string ProductSeName { get; set; } // SEO-friendly name for URL
+    }
+
+    public class SpecialOffersListModel
+    {
+        public IList<SpecialOfferModel> Offers { get; set; } = new List<SpecialOfferModel>();
+    }
+}
